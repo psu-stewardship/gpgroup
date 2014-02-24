2fe78412d1c17df1344045159041833979cd6781
Scott Woods <scott@westarete.com>
2014-02-23 21:51:58 -0500

    Have #init create an empty .gpg-known-keys file
    This demonstrates how to have the action do something, and how to have
    the specs test the result.

--- lib/gpgroup.rb
+++ lib/gpgroup.rb
@@ -1,9 +1,11 @@
 require 'thor'
 
 class GPGroup < Thor
+  include Thor::Actions
 
   desc "init", "Creates the initial .gpg-known-keys and .gpg-recipients"
   def init
+    create_file ".gpg-known-keys"
   end
 
 end
--- spec/gpgroup_spec.rb
+++ spec/gpgroup_spec.rb
@@ -2,16 +2,32 @@ require 'spec_helper'
 require 'gpgroup'
 
 describe GPGroup do
+  let(:args) { [] }
+  let(:options) {{ quiet: true }}
+  let(:gpgroup) { GPGroup.new(args, options) }
   it "inherits from Thor" do
     GPGroup.ancestors.should include(Thor)
   end
-  describe 'the init command' do
+  describe "the init command" do
     let(:command) { GPGroup.commands['init'] }
     it "is defined" do
       command.should_not be_nil
     end
     it "has a description" do
       command.description.should_not be_empty
+    end
+    context "when run inside a new directory" do
+      let(:tmp_dir) { $root.join("tmp") }
+      let(:repo_dir) { tmp_dir.join("repo") }
+      before do
+        FileUtils.rm_rf(tmp_dir)
+        FileUtils.mkpath(repo_dir)
+        FileUtils.chdir(repo_dir)
+        gpgroup.init
+      end
+      it "creates the .gpg-known-keys file" do
+        repo_dir.join(".gpg-known-keys").should exist
+      end
     end
   end
 end
