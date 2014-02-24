920482637378616ff5542fcd39d3d6f31d613840
Justin Patterson and Scott Woods <developers+jrp22+scott@westarete.com>
2014-02-24 15:31:46 -0500

    Add some spacing to improve readability

--- spec/gpgroup_spec.rb
+++ spec/gpgroup_spec.rb
@@ -5,17 +5,22 @@ describe GPGroup do
   let(:args) { [] }
   let(:options) {{ quiet: true }}
   let(:gpgroup) { GPGroup.new(args, options) }
+
   it "inherits from Thor" do
     GPGroup.ancestors.should include(Thor)
   end
+
   describe "the init command" do
     let(:command) { GPGroup.commands['init'] }
+
     it "is defined" do
       command.should_not be_nil
     end
+
     it "has a description" do
       command.description.should_not be_empty
     end
+
     context "when run inside a new directory" do
       let(:tmp_dir) { $root.join("tmp") }
       let(:repo_dir) { tmp_dir.join("repo") }
@@ -25,9 +30,11 @@ describe GPGroup do
         FileUtils.chdir(repo_dir)
         gpgroup.init
       end
+
       it "creates the .gpg-known-keys file" do
         repo_dir.join(".gpg-known-keys").should exist
       end
+
     end
   end
 end
